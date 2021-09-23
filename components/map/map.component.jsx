f18fdc16b0410ef11aaf973f7fafee0152a3bb07
NoelCov <73734505+NoelCov@users.noreply.github.com>
2021-09-23 11:17:56 -0700

    Removed console.log from map component

--- components/map/map.component.jsx
+++ components/map/map.component.jsx
@@ -28,5 +28,3 @@ class MapContainer extends Component {
 export default GoogleApiWrapper({
   apiKey: process.env.NEXT_PUBLIC_GOOGLE_KEY,
 })(MapContainer);
-
-console.log(process.env.NEXT_PUBLIC_GOOGLE_KEY)
