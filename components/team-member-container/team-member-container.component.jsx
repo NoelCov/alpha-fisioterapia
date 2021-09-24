cab4ad6fdaf95d57bfb6cba11cbce198d1f825a9
NoelCov <73734505+NoelCov@users.noreply.github.com>
2021-09-23 20:28:11 -0700

    Modified text in app

--- components/team-member-container/team-member-container.component.jsx
+++ components/team-member-container/team-member-container.component.jsx
@@ -39,33 +39,39 @@ const TeamMemberContainerRight = ({ img, name, text }) => {
   );
 };
 
+const alejandraText = `Gran parte de mi vida he practicado y he aprendido diversos deportes
+como el atletismo, fútbol, calistenia y yoga. Gracias a que soy una persona apasionada por el movimiento y
+la actividad física he logrado implementar el ejercicio con mis pacientes para enseñarles
+el mejor camino para tratar su recuperación, tratando y enseñándoles sobre su dolor, limitación y lesiones. Para 
+llegar a todo aquel que sufra de dolor y quiera vivir plenamente. Me encanta moverme, aprender y enseñar a través del
+ movimiento.`;
+
+const luisText = `Fisioterapeuta, atleta de crossfit y judoka. Toda mi vida he buscado ser una persona
+dinámica. Me interesan los cambios y los desafíos que estos puedan representar.
+A través de la fisioterapia busco mantener a nuestros clientes sanos, activos y en movimiento.`;
+
+const davidText = `Fisioterapeuta, docente y practicante del deporte de fuerza, con un claro enfoque en la
+mejora constante y el aprendizaje, tanto personal y profesional así como con nuestros pacientes, en
+la búsqueda de la adquisición y mejoramiento de las capacidades físicas a través del movimiento y la
+re-educación.`;
+
 export const BigContainer = () => {
   return (
     <ContentContainer>
       <TeamMemberContainerLeft
         img="/images/team-members/alpha-ale.jpg"
         name="Alejandra"
-        text={`Gran parte de mi vida he practicado y he aprendido diversos deportes
-            como el atletismo, fútbol, calistenia y yoga. Gracias a que soy una persona apasionada por el movimiento y
-            la actividad física he logrado implementar el ejercicio con mis pacientes para enseñarles
-            el mejor camino para tratar su recuperación, tratando y enseñándoles sobre su dolor, limitación y lesiones. Para 
-            llegar a todo aquel que sufra de dolor y quiera vivir plenamente. Me encanta moverme, aprender y enseñar a través del
-             movimiento.`}
+        text={alejandraText}
       />
       <TeamMemberContainerRight
         img="/images/team-members/alpha-david.jpg"
         name="David"
-        text={`Fisioterapeuta, docente y practicante del deporte de fuerza, con un claro enfoque en la
-              mejora constante y el aprendizaje, tanto personal y profesional así como con nuestros pacientes, en
-              la búsqueda de la adquisición y mejoramiento de las capacidades físicas a través del movimiento y la
-              re-educación.`}
+        text={davidText}
       />
       <TeamMemberContainerLeft
         img="/images/team-members/alpha-luis.jpg"
         name="Luis"
-        text={`Fisioterapeuta, atleta de crossfit y judoka. Toda mi vida eh buscado ser una persona
-              dinámica. Me interesan los cambios y los desafíos que estos puedan representar.
-              A través de la fisioterapia busco mantener a nuestros clientes sanos, activos y en movimiento.`}
+        text={luisText}
       />
     </ContentContainer>
   );
@@ -77,27 +83,17 @@ export const SmallContainer = () => {
       <TeamMemberContainerLeft
         img="/images/team-members/alpha-ale.jpg"
         name="Alejandra"
-        text={`Gran parte de mi vida he practicado y eh aprendido diversos deportes
-            como el atletismo, fútbol, calistenia y yoga. Gracias a que siempre eh sido una
-            apasionada por el movimiento y actividad física; eh podido implementarlo con mis pacientes, para enseñarles
-            el mejor camino para tratar su dolor, limitación, para que conozcan y tratemos juntos sus lesiones.
-            Para llegar a todo aquel que sufra de dolor y quiera seguir practicando aquello que les guste. Me 
-            encanta moverme, aprender y ensenar a través del movimiento.`}
+        text={alejandraText}
       />
       <TeamMemberContainerLeft
         img="/images/team-members/alpha-david.jpg"
         name="David"
-        text={`Fisioterapeuta, docente y practicante del deporte de fuerza, con un claro enfoque en la
-              mejora constante y el aprendizaje, tanto personal y profesional así como con nuestros pacientes, en
-              la búsqueda de la adquisición y mejoramiento de las capacidades físicas a través del movimiento y la
-              re-educación`}
+        text={davidText}
       />
       <TeamMemberContainerLeft
         img="/images/team-members/alpha-luis.jpg"
         name="Luis"
-        text={`Fisioterapeuta, atleta de crossfit y judoka. Toda mi vida eh buscado ser una persona
-              dinámica. Me interesan los cambios y los desafíos que estos puedan representar.
-              A través de la fisioterapia busco mantener a nuestros clientes sanos, activos y en el movimiento`}
+        text={luisText}
       />
     </ContentContainer>
   );
